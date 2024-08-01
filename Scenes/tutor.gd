b74a1f86c9068a2f38fcc43c91ccf0315cf91b73
blackbrokkoli <kastomat@gmail.com>
2024-08-01 19:03:57 +0300

    make the rabbit move to the box

--- Scenes/tutor.gd
+++ Scenes/tutor.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed : float = 200.0
+@export var speed : float = 100.0
 @export var jump_velocity : float = -150.0
 @export var double_jump_velocity : float = -100
 
@@ -8,75 +8,43 @@ extends CharacterBody2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_double_jumped : bool = false
-var animation_locked : bool = false
 var direction : Vector2 = Vector2.ZERO
-var was_in_air : bool = false
+
+
+@export var target: Node2D
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		was_in_air = true
-	else:
-		has_double_jumped = false
-		
-		if was_in_air == true:
-			land()
-			
-		was_in_air = false
 
-	# # Handle Jump.
-	# if Input.is_action_just_pressed("jump"):
-	# 	if is_on_floor():
-	# 		# Normal jump from floor
-	# 		jump()
-	# 	elif not has_double_jumped:
-	# 		# Double jump in air
-	# 		double_jump()
-			
+	# if target set, move towards target
+	# this is an NPC, NOT player-controlled at all
+	if target:
+		direction = target.global_position - global_position
+		direction = direction.normalized()
+		velocity.x = direction.x * speed
+		velocity.y = direction.y * speed
+		move_and_slide()
+		update_animation()
+		update_facing_direction()
+		# if closer than 10 units, stop
+		# set target to null, go idle
+		if global_position.distance_to(target.global_position) < 10:
+			target = null
+			velocity = Vector2.ZERO
+			animated_sprite.play("idle")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	# direction = Input.get_vector("left", "right", "up", "down")
-	
-	# if direction.x != 0 && animated_sprite.animation != "jump_end":
-	# 	velocity.x = direction.x * speed
-	# else:
-	# 	velocity.x = move_toward(velocity.x, 0, speed)
-
-	velocity.x = move_toward(velocity.x, 0, speed)
-
-	move_and_slide()
-	update_animation()
-	update_facing_direction()
 	
 func update_animation():
-	if not animation_locked:
-		if not is_on_floor():
-			animated_sprite.play("jump_loop")
-		else:
-			if direction.x != 0:
-				animated_sprite.play("run")
-			else:
-				animated_sprite.play("idle")
+	if direction.x != 0:
+		animated_sprite.play("run")
+	else:
+		animated_sprite.play("idle")
 
 func update_facing_direction():
 	if direction.x > 0:
 		animated_sprite.flip_h = false
 	elif direction.x < 0:
 		animated_sprite.flip_h = true
-		
-func jump():
-	velocity.y = jump_velocity
-	animated_sprite.play("jump_start")
-	animation_locked = true
-
-func land():
-	animated_sprite.play("jump_end")
-	animation_locked = true
-
-func _on_animated_sprite_2d_animation_finished():
-	if(["jump_end", "jump_start", "jump_double"].has(animated_sprite.animation)):
-		animation_locked = false
 		
