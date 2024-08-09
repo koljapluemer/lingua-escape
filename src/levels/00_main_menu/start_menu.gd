03185fc8856f2c884ae73503062a38ff6b563ba6
blackbrokkoli <kastomat@gmail.com>
2024-08-09 20:50:29 +0300

    reestablish start menu

--- src/levels/00_main_menu/start_menu.gd
+++ src/levels/00_main_menu/start_menu.gd
@@ -1,5 +1,4 @@
 extends Control
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,4 +10,4 @@ func _process(delta: float) -> void:
 
 
 func _on_button_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/test_level.tscn")
+	get_tree().change_scene_to_file("res://src/levels/01_two_objects_walk/map.tscn")
